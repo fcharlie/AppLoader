30f0c796e1dc2e09af8b44ac3709ed6d75f69ed9
Force Charlie <charlieio@outlook.com>
2016-03-27 22:28:49 +0800

    first commit

---
+++ ReadMe.md
@@ -0,0 +1,3 @@
+# App Loader
+This is Native App Loader, Auto Set App Environment
+
---
+++ soruces/main.cc
@@ -0,0 +1,12 @@
+//
+//
+//
+#include <cstdint>
+#include <cstdlib>
+#include <cstdio>
+#include <windows.h>
+
+int WINAPI wWinMain(HINSTANCE ,HINSTANCE, LPWSTR ,int)
+{
+	return 0;
+}
