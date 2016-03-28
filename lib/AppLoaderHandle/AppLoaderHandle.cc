935413902fa51df63703aa18d6df1f35923c6869
Force Charlie <ipvb@qq.com>
2016-03-28 19:01:05 +0800

    Dll File add

--- lib/AppLoaderHandle/AppLoaderHandle.cc
+++ lib/AppLoaderHandle/AppLoaderHandle.cc
@@ -1,2 +1,25 @@
-//https://msdn.microsoft.com/en-us/library/windows/desktop/cc144122(v=vs.85).aspx
+/*
+*
+*/
 
+// https://msdn.microsoft.com/en-us/library/windows/desktop/cc144122(v=vs.85).aspx
+
+#include <stdio.h>
+#include <windows.h>
+
+BOOL WINAPI DllMain(HINSTANCE hInstance, DWORD fdwReason, LPVOID lpReserved) {
+  switch (fdwReason) {
+  case DLL_PROCESS_ATTACH:
+    break;
+  case DLL_THREAD_ATTACH:
+    break;
+  case DLL_THREAD_DETCH:
+    break;
+  case DLL_PROCESS_DETCH:
+    break;
+  default:
+    break;
+  }
+  /// To do
+  return true;
+}
